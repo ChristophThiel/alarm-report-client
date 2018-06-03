a7feb0c7eae2d2b8c265d8636c8416cad889eaa5
ChristophThiel <christoph.thiel@liwest.at>
2018-06-03 17:08:05 +0200

    Usage of icons instead of text for buttons, show keyword and street on the toolbar and modify side nav

--- src/app/components/app/app.component.ts
+++ src/app/components/app/app.component.ts
@@ -11,9 +11,14 @@ import { RestService } from '../../services/rest.service';
 export class AppComponent {
   
   public currentAlarm: Alarm;
+  public alarms: string[];
 
   constructor(private rest: RestService) { 
     this.currentAlarm = new Alarm();
+    this.currentAlarm.alarmKeyword = "Türöffnung";
+    this.currentAlarm.street = "Berggasse 4";
+
+    this.alarms = [];
   }
 
   public generatePdf(): void {
