62678156e48f58427f3818bf8caa041d3d1a1d96
ChristopThiel <christoph.thiel@liwest.at>
2018-06-26 22:14:37 +0200

    Push Protocol Component

---
+++ src/app/components/protocol/protocol.component.ts
@@ -0,0 +1,14 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-protocol',
+  templateUrl: './protocol.component.html',
+  styleUrls: ['./protocol.component.css']
+})
+export class ProtocolComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() { }
+
+}
