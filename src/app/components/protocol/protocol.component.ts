6d451ebb9ee492a0bb3470cdd6c8b22eb27245d6
ChristopThiel <christoph.thiel@liwest.at>
2018-06-27 15:05:37 +0200

    Add edit function to Protocol Component

--- src/app/components/protocol/protocol.component.ts
+++ src/app/components/protocol/protocol.component.ts
@@ -15,9 +15,20 @@ export class ProtocolComponent implements OnInit {
   private displayedColumns = ["time", "text", "edit", "delete"];
   private entries: Entry[] = [new Entry("12:32", "Meldung laut EL, Fehlalarm, Einsatzende")];
 
-  constructor() { }
+  constructor(private dialog: MatDialog) { }
 
   ngOnInit() { }
+
+  private openDialog(entry: Entry): void {
+    let dialogRef = this.dialog.open(ProtocolDialog, {
+      width: '40%',
+      data: { id: this.entries.indexOf(entry), time: entry.time, text: entry.text }
+    });
+
+    dialogRef.afterClosed().subscribe(result => {
+      this.update(result);
+    });
+  }
 
   private add(): void {
     this.entries = this.entries.concat(new Entry(this.time, this.text));
@@ -26,6 +37,12 @@ export class ProtocolComponent implements OnInit {
   private delete(entry: Entry): void {
     let deleted = this.entries.splice(this.entries.indexOf(entry), 1);
     this.entries = this.entries.filter(member => member != deleted[0]);
+  }
+
+  private update(entry: any): void {
+    let update = this.entries.slice(0, this.entries.length - 1);
+    update[entry.id] = new Entry(entry.time, entry.text);
+    this.entries = update;
   }
 
 }
