d7aae87c69ae3a67a805c65e33300dfc9b2d8166
ChristophThiel <christoph.thiel@liwest.at>
2020-08-09 11:38:19 +0200

    Update angular to 9.1.12

--- src/app/shared/custom.matcher.ts
+++ src/app/shared/custom.matcher.ts
@@ -1,6 +1,8 @@
 import { ErrorStateMatcher } from '@angular/material/core';
 import { FormControl, FormGroupDirective, NgForm } from '@angular/forms';
+import { Injectable } from "@angular/core";
 
+@Injectable()
 export class CustomErrorStateMatcher implements ErrorStateMatcher {
   public isErrorState(control: FormControl, form: FormGroupDirective | NgForm): boolean {
     return (control.dirty || control.touched) && control.invalid;
